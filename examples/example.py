57955818572f5f6c51917340f63a08296f249f8f
William E. Robinson <werobinson6@gmail.com>
2025-04-03 12:18:23 +0200

    describe examples

--- examples/example.py
+++ examples/example.py
@@ -1,3 +1,8 @@
+"""
+Loading in a set of desired sample concentrations and proposed stock
+concentrations from a file, and optimising the stock concentrations to make the
+experiment feasible.
+"""
 import pandas as pd
 from dilution_solver import calculate_stock_volumes
 from dilution_solver import validate_or_optimize
